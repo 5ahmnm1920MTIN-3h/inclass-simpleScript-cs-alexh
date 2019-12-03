3ce7b9172738b082376df78dfe1ee5d23a3c1527
alexhoedlmoser <alex.hoedlmoser@gmail.com>
2019-12-03 17:40:32 +0100

    added multiply method to ComputeNumbers script, added toggle group to switch between multiply and subtract mode

--- Assets/MyGame/Scripts/ComputeNumbers.cs
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -7,19 +7,57 @@ using TMPro;
 public class ComputeNumbers : MonoBehaviour
 {
     public TextMeshProUGUI result;
+    public TextMeshProUGUI operatorSymbol;
     public TMP_InputField inputA;
     public TMP_InputField inputB;
     public Button btnReset;
-    public Button btnSubNumbers;
+    public Button btnCalculate;
+    public Toggle tglSubMode;
+    public Toggle tglMultMode;
 
     public void SetResult()
     {
-        result.text = SubNumbers().ToString();
-        //set gui-elements active/inactive 
+        if (operatorSymbol.text == "-")
+        {
+            result.text = SubNumbers().ToString();
+        }
+
+        if (operatorSymbol.text == "*")
+        {
+            result.text = MultNumbers().ToString();
+        }
+
+        DisableInput();
+    }
+
+    private void DisableInput()
+    {
         inputA.interactable = false;
         inputB.interactable = false;
-        btnSubNumbers.interactable = false;
+        btnCalculate.interactable = false;
         btnReset.interactable = true;
+        tglSubMode.interactable = false;
+        tglMultMode.interactable = false;
+    }
+
+    private void EnableInput()
+    {
+        inputA.interactable = true;
+        inputB.interactable = true;
+        btnCalculate.interactable = true;
+        btnReset.interactable = false;
+        tglSubMode.interactable = true;
+        tglMultMode.interactable = true;
+    }
+
+    public void MultOperator()
+    {
+        operatorSymbol.text = "*";
+    }
+
+    public void SubOperator()
+    {
+        operatorSymbol.text = "-";
     }
 
     public void Reset()
@@ -27,15 +65,19 @@ public class ComputeNumbers : MonoBehaviour
         inputA.text = "0";
         inputB.text = "0";
         result.text = "Result";
-        inputA.interactable = true;
-        inputB.interactable = true;
-        btnSubNumbers.interactable = true;
-        btnReset.interactable = false;
+        EnableInput();
+        
     }
 
     private int SubNumbers()
     {
         int tempResult = int.Parse(inputA.text) - int.Parse(inputB.text);
+        return tempResult;
+    }
+
+    private int MultNumbers()
+    {
+        int tempResult = int.Parse(inputA.text) * int.Parse(inputB.text);
         return tempResult;
     }
 
